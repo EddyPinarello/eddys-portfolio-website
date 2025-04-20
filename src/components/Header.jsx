20f9e89eed352ebc61e614e94501af88a05205c4
Eddy <149050257+EddyPinarello@users.noreply.github.com>
2025-04-21 01:07:21 +0200

    Initialize files

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav aria-label="Main Navigation">
+        <ul>
+          <li><a href="#portfolio">Eddy's Portfolio</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#work-experience">Work Experience</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
+  
