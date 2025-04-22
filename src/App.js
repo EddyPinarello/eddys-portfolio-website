f3659a0ce72dc8e61307bdb87f0545d16c366071
Eddy <149050257+EddyPinarello@users.noreply.github.com>
2025-04-22 15:44:55 +0200

    Adjusted some problems with mobiles, navbar done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import './styles/mobile.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,13 @@ function Header() {
   return (
     <header>
       <nav aria-label="Main Navigation">
-        <ul>
+        <ul className='navbar-items'>
           <li><a href="#portfolio">Eddy's Portfolio</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#work-experience">Work Experience</a></li>
+          <div className='navbar-pages'>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#work-experience">Work Experience</a></li>
+            <li><a href="#about">About</a></li>
+          </div>
         </ul>
       </nav>
     </header>
